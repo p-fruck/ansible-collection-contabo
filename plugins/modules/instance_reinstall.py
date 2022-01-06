bc586c21f467b4ffcb9a8b6c8e8426e05e2c47d8
Philipp Fruck <dev@p-fruck.de>
2022-01-06 13:04:37 +0100

    add instance_reinstall module

---
+++ plugins/modules/instance_reinstall.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from ansible.module_utils.basic import AnsibleModule
+from ansible_collections.pfruck.contabo.plugins.module_utils.contabo_rest_client import ContaboRestClient
+from pfruck_contabo import InstancesApi
+
+class ContaboInstanceInfo(ContaboRestClient):
+    def __init__(self, module):
+        super(ContaboInstanceInfo, self).__init__(module, InstancesApi)
+
+    def reinstall_instance(self):
+        instance_id = self.module.params.get("instance_id")
+        body = self.map_kwargs({ "image_id": "imageId", "user_data": "userData" })
+        api_response = self._request(self.api_instance.reinstall_instance, instance_id, body=body)
+        return self.format_json(api_response)
+
+def argspec():
+    return {
+        "api_key": {"type": "str", "required": True, "no_log": True},
+        "instance_id": {"type": "int", "required": True},
+        "image_id": {"type": "str", "required": True},
+        "user_data": {"type": "str", "required": False, "no_log": True},
+    }
+
+def main():
+    module = AnsibleModule(argument_spec=argspec(), supports_check_mode=False)
+    cntb_mgr = ContaboInstanceInfo(module)
+    data = cntb_mgr.reinstall_instance()
+    module.exit_json(msg=data)
+
+if __name__ == '__main__':
+    main()
