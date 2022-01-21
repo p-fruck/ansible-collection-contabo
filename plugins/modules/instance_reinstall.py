b0e29b0180532c16043686bbf4f5cbd41c14dd57
Philipp Fruck <dev@p-fruck.de>
2022-01-21 13:44:00 +0100

    add root password and ssh key parameters to instance_reinstall

--- plugins/modules/instance_reinstall.py
+++ plugins/modules/instance_reinstall.py
@@ -11,7 +11,7 @@ class ContaboInstanceInfo(ContaboRestClient):
 
     def reinstall_instance(self):
         instance_id = self.module.params.get("instance_id")
-        body = self.map_kwargs({ "image_id": "imageId", "user_data": "userData" })
+        body = self.map_kwargs({ "image_id": "imageId", "user_data": "userData", "root_password_id": "rootPassword", "ssh_key_ids": "sshKeys" })
         api_response = self._request(self.api_instance.reinstall_instance, instance_id, body=body)
         return self.format_json(api_response)
 
@@ -20,6 +20,8 @@ def argspec():
         "api_key": {"type": "str", "required": True, "no_log": True},
         "instance_id": {"type": "int", "required": True},
         "image_id": {"type": "str", "required": True},
+        "root_password_id": {"type": "int"},
+        "ssh_key_ids": {"type": "list", "elements": "int"},
         "user_data": {"type": "str", "required": False, "no_log": True},
     }
 
@@ -27,7 +29,7 @@ def main():
     module = AnsibleModule(argument_spec=argspec(), supports_check_mode=False)
     cntb_mgr = ContaboInstanceInfo(module)
     data = cntb_mgr.reinstall_instance()
-    module.exit_json(msg=data)
+    module.exit_json(data=data)
 
 if __name__ == '__main__':
     main()
